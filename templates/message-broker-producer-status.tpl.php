ddbc87fabee8d129c0553ee48b6719016deb4e4f
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-02-27 11:28:29 -0500

    Adds test transaction producer link to admin

--- templates/message-broker-producer-status.tpl.php
+++ templates/message-broker-producer-status.tpl.php
@@ -1,12 +1,10 @@
-<?php
-  $bla = TRUE;
-  if ($bla) {
-    $bla = FALSE;
-  }
-?>
 <h1>RabbitMQ Status</h1>
 <ul>
-<?php foreach ($output as $status_type => $status): ?>
+<?php foreach ($status as $status_type => $status): ?>
   <li><strong><?php print ucwords($status_type) ?></strong>: <?php print $status ?></li>
 <?php endforeach; ?>  
+</ul>
+<h3>Tests</h3>
+<ul>
+  <li><?php print $test_link ?></li>
 </ul>
