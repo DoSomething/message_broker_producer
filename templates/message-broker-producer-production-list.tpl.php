f058b7c71d865b7f861a65af94a22739c68f146f
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-10-19 16:54:38 -0400

    Adds /list-productions framework

---
+++ templates/message-broker-producer-production-list.tpl.php
@@ -0,0 +1,9 @@
+<h1>Productions</h1>
+<p>Possible production request types for message_broker_producter_request() types.</p>
+<table></table>
+<?php foreach ($productions as $production_id => $production): ?>
+  <tr>
+    <td><a href=""></a><?php echo $production['machine_name'] ?></a></td>
+    <td>Delete</td>
+  </tr>
+<?php endforeach; ?>
