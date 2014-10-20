fbd6a879371c62d5175d1d20e4118b949f1310ae
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-10-19 20:31:37 -0400

    Queues in production listing

--- templates/message-broker-producer-production-list.tpl.php
+++ templates/message-broker-producer-production-list.tpl.php
@@ -1,3 +1,6 @@
 <h1>Productions</h1>
 <p>Possible production request types for message_broker_producter_request() types.</p>
+<ul>
+  <li><a href="/admin/config/services/message-broker-producer/add-edit-production">Add Production</a></li>
+</ul>
 <?php echo $productions ?>
