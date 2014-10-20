0261071845a0a25abbcc75f7f0c27b2a0bcd8e0c
Darren Douglas Lee aka Dee <dlee@dosomething.org>
2014-10-19 20:06:41 -0400

    Productions listing

--- templates/message-broker-producer-production-list.tpl.php
+++ templates/message-broker-producer-production-list.tpl.php
@@ -1,9 +1,3 @@
 <h1>Productions</h1>
 <p>Possible production request types for message_broker_producter_request() types.</p>
-<table></table>
-<?php foreach ($productions as $production_id => $production): ?>
-  <tr>
-    <td><a href=""></a><?php echo $production['machine_name'] ?></a></td>
-    <td>Delete</td>
-  </tr>
-<?php endforeach; ?>
+<?php echo $productions ?>
